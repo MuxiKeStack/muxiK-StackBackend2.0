146f50397d5977c5b1fb61e4765191243a70157b
Wishforpeace <1903183040@qq.com>
2023-08-13 11:47:21 +0800

    refactor:delete daily_integral.sql

--- deploy/sql/daily_integral.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `daily_integral`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `sid` VARCHAR(10)  NOT NULL UNIQUE COMMENT '学生学号',
-    `like_integral` INT NOT NULL DEFAULT 0 COMMENT'点赞积分',
-    `comment_integral` INT NOT NULL DEFAULT 0 COMMENT'评论积分',
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `idx_sid_unique`(`sid`)
-)ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
